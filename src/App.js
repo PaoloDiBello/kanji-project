67f9f3dcd68966e868d089a9d679e6c0a3aa336d
Paolo Di Bello <dibellopol@gmail.com>
2019-10-31 10:41:16 +0100

    remove Router from App.js

--- src/App.js
+++ src/App.js
@@ -1,41 +1,24 @@
-import React, {lazy, Suspense} from 'react';
-import './App.css';
+import React, { lazy, Suspense } from "react";
+import "./App.css";
 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
+const ListItems = lazy(() => import("./components/ListItems/ListItems"));
 
-const ListItems = (
-  lazy(() => (
-    import('./components/ListItems/ListItems')
-  ))
-)
+const SingleItem = lazy(() => import("./components/SingleItem/SingleItem"));
 
-const SingleItem = (
-  lazy(() => (
-    import('./components/SingleItem/SingleItem')
-  ))
-)
-
-const NotFound = (
-  lazy(() => (
-    import('./components/404')
-  ))
-)
-
+const NotFound = lazy(() => import("./components/404"));
 
 function App() {
-
   return (
-      <Router>
-      <Suspense fallback={<>loading..</>}>
-        <Switch>
+    <Suspense fallback={<>loading..</>}>
+      <Switch>
         <Route exact path="/info/:item" component={SingleItem} />
         <Route path="/" exact component={ListItems} />
         <Route path="/404" component={NotFound} />
         <Route path="*" component={NotFound} />
-        </Switch>
-        </Suspense>
-      </Router>
+      </Switch>
+    </Suspense>
   );
 }
 
